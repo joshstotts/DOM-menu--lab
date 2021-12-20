436d30778452136a471d1ab6284928305666d497
joshstotts <94932808+joshstotts@users.noreply.github.com>
2021-12-20 08:33:15 -0600

    Add files via upload

---
+++ dom-menu-lab/js/script.js
@@ -0,0 +1,24 @@
+// Menu data structure
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+let mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+mainEl.classList.add ('flex-ctr');
+
+let topMenuEl = document.querySelector('#top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add ('flex-around');
+
+menuLinks.forEach(function(a) {
+  let aEl = document.createElement('a');
+    aEl.setAttribute("href", a.href);
+    aEl.innerHTML(a.text);
+    aEl.append(topMenuEl);
+});
